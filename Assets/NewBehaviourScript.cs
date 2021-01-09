040b5fe179449ac6e5f1cd2bc42fd12c1cf16ee8
Darksw20 <ricardog.navi20@gmail.com>
2021-01-09 13:42:28 -0600

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Este es un pequeño cambio   
     }
 
     // Update is called once per frame
