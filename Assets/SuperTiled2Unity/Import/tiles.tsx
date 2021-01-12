5dd3173015e78b17343bf4e807de6963a26e6bb2
Darksw20 <ricardog.navi20@gmail.com>
2021-01-12 15:08:51 -0600

    added supertiled
    Pruebas de agregar un mapa

---
+++ Assets/SuperTiled2Unity/Import/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles" tilewidth="64" tileheight="64" tilecount="108" columns="12">
+ <image source="tilesheet_64x64.png" width="800" height="600"/>
+</tileset>
