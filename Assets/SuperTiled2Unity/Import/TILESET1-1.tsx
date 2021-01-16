63c657738520c792aac9699a39b3a819a620c4fc
Rosa Ramírez <rramirezcisneros@gmail.com>
2021-01-16 14:51:10 -0600

    Top down

---
+++ Assets/SuperTiled2Unity/Import/TILESET1-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="TILESET1-1" tilewidth="34" tileheight="34" tilecount="600" columns="15">
+ <image source="TILESET1-1.png" width="510" height="1360"/>
+</tileset>
