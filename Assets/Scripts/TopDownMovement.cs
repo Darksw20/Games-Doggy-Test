228153b6897ccb1679f55e4f2d577e54ac2f60c7
Darksw20 <ricardog.navi20@gmail.com>
2021-01-17 16:53:18 -0600

    Basic and Buggie topdown Movement

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    public Animator animator;
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        animator.SetFloat("Vertical", 0.0f, Input.GetAxis("Vertical"), 0.0f);
+
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + vertical * Time.deltaTime;
+
+    }
+}
