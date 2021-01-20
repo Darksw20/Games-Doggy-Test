57ba50f53d538696e9a9656ee069b67f6fcc42e7
Darksw20 <ricardog.navi20@gmail.com>
2021-01-20 17:52:06 -0600

    Animation fixed

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,14 +10,15 @@ public class TopDownMovement : MonoBehaviour
     void Update()
     {
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        animator.SetFloat("Vertical", 0.0f, Input.GetAxis("Vertical"), 0.0f);
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"),0.0f, 0.0f);
 
 
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal* speed * Time.deltaTime;
-
+        
         Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
         transform.position = transform.position + vertical* speed * Time.deltaTime;
 
+        Debug.Log("x=" + horizontal + "  y=" + vertical);
     }
 }
