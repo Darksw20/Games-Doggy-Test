321fd998ae2faa33a94851e967caeefca68129da
Darksw20 <ricardog.navi20@gmail.com>
2021-01-20 17:21:41 -0600

    Fixed speed and colliders in scene 2

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
+    public float speed = 3.0f;
     public Animator animator;
     void Update()
     {
@@ -12,10 +13,10 @@ public class TopDownMovement : MonoBehaviour
 
 
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        transform.position = transform.position + horizontal* speed * Time.deltaTime;
 
         Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
-        transform.position = transform.position + vertical * Time.deltaTime;
+        transform.position = transform.position + vertical* speed * Time.deltaTime;
 
     }
 }
